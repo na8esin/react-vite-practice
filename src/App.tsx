67777b98c0469a064a80a197602113d52cce248f
sikmi_watanabe <na8esin.mpst@gmail.com>
2024-05-20 09:08:25 +0900

    手番の処理

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,29 @@ interface SquareProps {
 }
 
 function Square({value, onSquareClick}: SquareProps) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i: number) {
+    if (squares[i]) return;
+
     // コピーするだけ
-    const newSquares = squares.slice();
-    newSquares[i] = 'X';
-    setSquares(newSquares);
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
